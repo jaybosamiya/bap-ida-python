91c3da9ed45b258d88d0a0ebae07a23c7a84d030
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-13 16:16:23 -0400

    Use cfg directory

--- plugins/bap/utils/config.py
+++ plugins/bap/utils/config.py
@@ -1,28 +1,28 @@
+"""Module for reading from and writing to the bap.cfg config file."""
+
 import os
 
-"""Package all configuration related methods together."""
-
-_dir = os.path.join(os.getenv('HOME'), '.bap')
-_file_path = os.path.join(_dir, 'config')
+cfg_dir = idaapi.idadir('cfg')
+cfg_path = os.path.join(cfg_dir, 'bap.cfg')
 
 def get(key, default=None):
     """Get value from key:value in the config file."""
     from bap.utils import bap_comment
-    if not os.path.exists(_file_path):
+    if not os.path.exists(cfg_path):
         return default
-    with open(_file_path, 'r') as f:
+    with open(cfg_path, 'r') as f:
         return bap_comment.get_value(f.read(), key, default)
 
 def set(key, value):
     """Set key:value in the config file."""
     from bap.utils import bap_comment
     try:
-        with open(_file_path, 'r') as f:
+        with open(cfg_path, 'r') as f:
             s = f.read()
     except IOError:
         s = ''
     s = bap_comment.add_to_comment_string(s, key, value)
-    if not os.path.exists(_dir):
-        os.makedirs(_dir)
-    with open(_file_path, 'w') as f:
+    if not os.path.exists(cfg_dir):
+        os.makedirs(cfg_dir)
+    with open(cfg_path, 'w') as f:
         f.write(s)
